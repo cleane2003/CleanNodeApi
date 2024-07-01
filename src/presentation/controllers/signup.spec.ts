ff0d28ffbf9b4f5b2411eb76f3bfbce2b5f1cdbd
João Pedro S Souza <joao.pedro@modalgr.io>
2024-06-30 22:48:19 -0300

    refactor: ensure SignUpController.handle implement correct interfaces

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -11,8 +11,8 @@ describe('SignUp Controller', () => {
       }
     }
     const httpResponse = sut.handle(httpRequest)
-    expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.body).toEqual(new Error('Missing param: name'))
+    expect(httpResponse?.statusCode).toBe(400)
+    expect(httpResponse?.body).toEqual(new Error('Missing param: name'))
   })
 
   it('Should return 400 if no email is provided', () => {
